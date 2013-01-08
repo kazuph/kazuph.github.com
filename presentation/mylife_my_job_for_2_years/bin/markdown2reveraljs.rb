d61e50f03809ab450c3db7c3f000f051fe4e98f9
Kazuhiro Homma <kazu.homma@gmail.com>
2013-01-08 18:07:52 +0900

    新しく作成

---
+++ presentation/mylife_my_job_for_2_years/bin/markdown2reveraljs.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# coding : utf-8
+require 'redcarpet'
+require 'erb'
+# define markdown
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(), :autolink => true,  :space_after_headers => true, :fenced_code_blocks => true, :no_intra_emphasis => true)
+# render html
+html = markdown.render(File.read('README.md'))
+
+# set title
+title = $1 if html =~ /<h1>(.+?)<\/h1>/
+
+# for reveal.js
+html.gsub!(/<h(\d)>/) { "</section><section>\n<h" + $1 + ">"}
+html.sub!(/<\/section>/, "")
+html = html + "</section>"
+
+html.gsub!(/-&gt;(.+?)(<\/p>)?$/, "<ul><li class=\"fragment\">\\1</li></ul>\\2")
+
+puts html
+
+# embed html
+html = ERB.new(File.read('template.html.erb')).result binding
+# puts html
+# write file
+open("index.html", "w") {|f| f.write html}
